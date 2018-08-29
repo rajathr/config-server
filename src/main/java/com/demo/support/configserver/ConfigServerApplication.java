85667e7c0868fc1fecf34b19022b3dbaa1e5fbe2
Rajath Rajagopal <P7111648@NESSINBLR.COM>
2018-08-29 11:29:32 +0530

    Added config server based settings

--- src/main/java/com/demo/support/configserver/ConfigServerApplication.java
+++ src/main/java/com/demo/support/configserver/ConfigServerApplication.java
@@ -1,9 +1,13 @@
 package com.demo.support.configserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
+@Slf4j
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
